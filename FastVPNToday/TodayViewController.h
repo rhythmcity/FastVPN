ad326356c2531f164af48377570e8e01b11b95b1
李言 <707704996@qq.com>
2016-04-05 18:13:26 +0800

    创建today

---
+++ FastVPNToday/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  FastVPNToday
+//
+//  Created by 李言 on 16/4/5.
+//  Copyright © 2016年 李言. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
---
+++ FastVPNToday/TodayViewController.m
@@ -0,0 +1,47 @@
+//
+//  TodayViewController.m
+//  FastVPNToday
+//
+//  Created by 李言 on 16/4/5.
+//  Copyright © 2016年 李言. All rights reserved.
+//
+
+#import "TodayViewController.h"
+#import <NotificationCenter/NotificationCenter.h>
+
+@interface TodayViewController () <NCWidgetProviding>
+
+@end
+
+@implementation TodayViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    self.view.backgroundColor = [UIColor redColor];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
+    // Perform any setup necessary in order to update the view.
+    
+    // If an error is encountered, use NCUpdateResultFailed
+    // If there's no update required, use NCUpdateResultNoData
+    // If there's an update, use NCUpdateResultNewData
+
+    completionHandler(NCUpdateResultNewData);
+}
+
+- (UIEdgeInsets)widgetMarginInsetsForProposedMarginInsets:(UIEdgeInsets)defaultMarginInsets{
+
+    return UIEdgeInsetsZero;
+
+}
+
+@end
